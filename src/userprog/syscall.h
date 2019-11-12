7ec9be46bfeea3a15a5c319577014f8e6ad5536a
undergraduate <cse20141506@cspro9.sogang.ac.kr>
2019-11-12 04:27:37 -0800

    2-2 start

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -13,6 +13,11 @@ int wait(pid_t pid);
 int read(int fd, void *buffer, unsigned size);
 int write(int fd, const void *buffer, unsigned size);
 
+/*2_2 added userprog*/
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+
+
 //user defined system call functions;
 int fibonacci(int n);
 int sum_of_four_int(int a, int b, int c, int d);
